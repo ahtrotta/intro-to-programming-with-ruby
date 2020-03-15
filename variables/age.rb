594fe895ca7644bbcc58e2f9654dae34ab0ff95f
Adam Trotta <ahtrotta@gmail.com>
2020-03-15 12:10:29 -0400

    completed exercises in variables chapter

---
+++ variables/age.rb
@@ -0,0 +1,4 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+
+[10, 20, 30, 40].each { |n| puts "In #{n} years you will be: \n#{n+age}" }
---
+++ variables/name.rb
@@ -0,0 +1,8 @@
+puts "Input your first name: "
+first_name = gets.chomp
+puts "Input your last name: "
+last_name = gets.chomp
+
+puts "Hello, #{first_name} #{last_name}!"
+
+10.times { puts first_name }

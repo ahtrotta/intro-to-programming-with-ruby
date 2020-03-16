5011c51573c34b268bb12b21ecb3a838c95c515c
Adam Trotta <ahtrotta@gmail.com>
2020-03-16 13:54:46 -0400

    completed exercises in arrays chapter

---
+++ arrays/exercise7.rb
@@ -0,0 +1,3 @@
+arr = ["corn", "beans", "cream", "butter", "apples", "carrots"]
+
+arr.each_with_index { |val, ind| puts "You can find #{val} at index #{ind}" }
---
+++ arrays/exercise8.rb
@@ -0,0 +1,6 @@
+original = [54, 83, 924, 3, 68, 239]
+
+new_arr = original.map { |n| n + 2 }
+
+p original
+p new_arr
